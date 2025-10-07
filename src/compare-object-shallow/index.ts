b9e52afb8cf421500fafa1c3cdb4ffa9542ad422
Vladyslav Bessmertnykh <bessvlad45@gmail.com>
2025-10-07 12:04:32 +0300

    feat(compare-object-shallow): Implement shallow equal function

--- src/compare-object-shallow/index.ts
+++ src/compare-object-shallow/index.ts
@@ -19,5 +19,20 @@ import { ShallowEqualFn } from './types';
  * ({ a: { x: 1 } }, { a: obj }) => true // if obj is same reference as value of a
  */
 export const shallowEqual: ShallowEqualFn = (a, b) => {
-  throw new Error('Not Implemented');
+  if (a === b) return true;
+
+  if (typeof a !== 'object' || typeof b !== 'object') return false;
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (!b.hasOwnProperty(key)) return false;
+
+    if (a[key] !== b[key]) return false;
+  }
+
+  return true;
 };
